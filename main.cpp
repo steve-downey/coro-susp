056432faaf7bc13572d613c3463e15da96ca9abe
Steve Downey <sdowney@gmail.com>
2017-04-19 18:45:05 -0400

    Non-default constructible

--- main.cpp
+++ main.cpp
@@ -1,14 +1,22 @@
 #include <experimental/coroutine>
 #include <future>
 #include <iostream>
+#include <functional>
+#include <atomic>
+#include <mutex>
 
 template <typename R>
 struct suspension {
+
   struct promise_type;
   using handle = std::experimental::coroutine_handle<promise_type>;
 
+  typedef typename std::aligned_storage<sizeof(R),
+                                        alignof(R)>::type
+  Storage;
+
   struct promise_type {
-    R result;
+    Storage result;
     using suspend_always = std::experimental::suspend_always;
     using suspend_never  = std::experimental::suspend_never;
 
@@ -25,76 +33,102 @@ struct suspension {
     }
 
     void return_value(R const& r) {
-      std::cout << "return value: " << r << '\n';
-      result = r;
+      //      *reinterpret_cast<R*>(&result) = r;
+      ::new (&result) R(r);
+    }
+    void unhandled_exception() {
+      std::terminate();
     }
   };
 
-  operator R() {
-    if (!_coro || _coro.done()) {
-      return _r;
+
+  operator R const&() {
+    return get();
+  }
+
+  R const& get() const {
+    int evaled = evaled_.load(std::memory_order_acquire);
+    if (!evaled) {
+      std::unique_lock<std::mutex> guard(lock_);
+      if (!evaled_) {
+        _coro.resume();
+        *reinterpret_cast<R*>(&result_) =
+          std::move(*reinterpret_cast<R*>(&_coro.promise().result));
+        _coro.destroy();
+        _coro = nullptr;
+        evaled_.store(1, std::memory_order_release);
+      }
     }
-    std::cout << "pre-resume\n";
-    _coro.resume();
-    std::cout << "after resume\n";
-    _r = _coro.promise().result;
-    std::cout << "about to destroy coro\n";
-    std::cout << "Pre Coro=" << (bool)_coro << '\n';
-    //
-    _coro.destroy();
-    _coro = nullptr;
-    std::cout << "Post Coro=" << (bool)_coro << '\n';
-    return _r;
+    return *reinterpret_cast<R*>(&result_);
   }
 
   suspension(handle h) : _coro(h) {
-    std::cout << "cons by handle\n";
   }
 
   suspension() : _coro(nullptr) {
-    std::cout << "default cons\n";
   }
 
   suspension(suspension&& rhs) : _coro(rhs._coro) {
     rhs._coro = nullptr;
-    std::cout << "move cons\n";
   }
 
   suspension(suspension const&) = delete;
 
   ~suspension() {
-    std::cout << "Destroy Coro=" << (bool)_coro << '\n';
     if (_coro) {
       _coro.destroy();
     }
   }
 
-  handle _coro;
-  R      _r;
+
+  mutable Storage result_;
+  mutable std::atomic_int evaled_;
+  mutable std::mutex lock_;
+  mutable handle _coro;
 };
 
 template <typename F, typename... Args>
 auto suspend(F&& f, Args&&... args)
     -> suspension<typename std::result_of_t<F(Args...)>> {
-  std::cout << "Args:";
-  (std::cout << ... << args) << '\n';
-  std::cout << "before co_return\n";
   co_return std::invoke(f, args...);
 }
 
 int f(int i) {
-  std::cout << "i:" << i << '\n';
+  //  std::cout << "i:" << i << '\n';
   return i;
 }
 
-int main(int, char**) {
-  std::cout << "main\n";
-  std::cout << "about to create suspension\n";
-  auto s = suspend(f, 2);
-  std::cout << "after suspension\n";
-  //    int i = s;
-  std::cout << s << '\n';
+namespace {
+struct NoDefault
+{
+  int _i;
+ public:
+  NoDefault(int i) : _i(i) {}
+  NoDefault() = delete;
+};
 
+}
+
+NoDefault noDefault() {
+  return NoDefault{7};
+}
+
+int main(int, char**) {
+  // std::cout << "main\n";
+  // std::cout << "about to create suspension\n";
+  auto s = suspend(f, 2);
+  // std::cout << "after suspension\n";
+  //    int i = s;
+  //std::cout << s << '\n';
+
+  // std::cout << "about to create suspension3\n";
   auto s3 = suspend(f, 3);
-  return s;
+  // std::cout << "after suspension\n";
+
+  // std::cout << "about to create suspension4\n";
+  auto s4 = suspend(noDefault);
+  //  std::cout << "after suspension\n";
+  //std::cout << s4.get()._i << '\n';
+
+  return s + s4.get()._i;
 }
