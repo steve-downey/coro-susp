d204bd26a7eb76926b9ea2cc0e048dbd4cfa7cd4
Steve Downey <sdowney@gmail.com>
2017-04-19 20:29:10 -0400

    Make shared state

--- main.cpp
+++ main.cpp
@@ -15,10 +15,35 @@ struct suspension {
                                         alignof(R)>::type
   Storage;
 
+  class state {
+   private:
+    Storage result_;
+   public:
+    state() {std::cout << "state\n";}
+    ~state() {std::cout << "~state\n";}
+
+    template <class Arg>
+    void set_value(Arg&& arg) {
+      ::new(&result_) R(std::forward<Arg>(arg));
+    }
+
+    typename std::add_lvalue_reference<R>::type
+    copy(){
+      return *reinterpret_cast<R*>(&result_);
+    }
+
+  };
+
   struct promise_type {
-    Storage result;
+    std::shared_ptr<state> stateX_;
     using suspend_always = std::experimental::suspend_always;
     using suspend_never  = std::experimental::suspend_never;
+
+    promise_type() : stateX_(nullptr) {}
+
+    void set_state(std::shared_ptr<state> state) {
+      stateX_ = state;
+    }
 
     suspend_always initial_suspend() {
       return {};
@@ -33,9 +58,13 @@ struct suspension {
     }
 
     void return_value(R const& r) {
-      //      *reinterpret_cast<R*>(&result) = r;
-      ::new (&result) R(r);
+      stateX_->set_value(r);
     }
+
+    void return_value(R&& r) {
+      stateX_->set_value(r);
+    }
+
     void unhandled_exception() {
       std::terminate();
     }
@@ -43,32 +72,51 @@ struct suspension {
 
 
   operator R const&() {
+    std::cout << "conv\n";
     return get();
   }
 
   R const& get() const {
     int evaled = evaled_.load(std::memory_order_acquire);
+    std::cout << "get evaled: " << evaled_ << "\n";
     if (!evaled) {
       std::unique_lock<std::mutex> guard(lock_);
+      std::cout << "got lock!\n";
       if (!evaled_) {
         _coro.resume();
-        *reinterpret_cast<R*>(&result_) =
-          std::move(*reinterpret_cast<R*>(&_coro.promise().result));
+        std::cout << "resume!\n";
         _coro.destroy();
+        std::cout << "destroy!\n";
         _coro = nullptr;
+        std::cout << "null!\n";
         evaled_.store(1, std::memory_order_release);
+        std::cout << "stored!\n";
       }
     }
-    return *reinterpret_cast<R*>(&result_);
+    return state_->copy();
   }
 
-  suspension(handle h) : _coro(h) {
+  suspension(handle h) : _coro(h),
+                         evaled_(0),
+                         state_(new state),
+                         lock_()
+  {
+    _coro.promise().set_state(state_);
   }
 
-  suspension() : _coro(nullptr) {
+  suspension() : _coro(nullptr),
+                 evaled_(0),
+                 state(nullptr),
+                 lock_()
+  {
   }
 
-  suspension(suspension&& rhs) : _coro(rhs._coro) {
+  suspension(suspension&& rhs)
+  : _coro(std::move(rhs._coro)),
+    evaled_(0),
+    state_(std::move(rhs.state_)),
+    lock_()
+  {
     rhs._coro = nullptr;
   }
 
@@ -81,10 +129,10 @@ struct suspension {
   }
 
 
-  mutable Storage result_;
-  mutable std::atomic_int evaled_;
-  mutable std::mutex lock_;
   mutable handle _coro;
+  mutable std::atomic_int evaled_;
+  mutable std::shared_ptr<state> state_;
+  mutable std::mutex lock_;
 };
 
 template <typename F, typename... Args>
@@ -118,8 +166,9 @@ int main(int, char**) {
   // std::cout << "about to create suspension\n";
   auto s = suspend(f, 2);
   // std::cout << "after suspension\n";
-  //    int i = s;
-  //std::cout << s << '\n';
+  int i = s;
+  std::cout << s << '\n';
+  std::cout << s << '\n';
 
   // std::cout << "about to create suspension3\n";
   auto s3 = suspend(f, 3);
@@ -128,7 +177,7 @@ int main(int, char**) {
   // std::cout << "about to create suspension4\n";
   auto s4 = suspend(noDefault);
   //  std::cout << "after suspension\n";
-  //std::cout << s4.get()._i << '\n';
+  std::cout << s4.get()._i << '\n';
 
-  return s + s4.get()._i;
+  //  return s + s4.get()._i;
 }
